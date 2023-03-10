51736671786cd66ab75aee4c1f38d087ee5cdbda
elamandeep <deep.aman1729@gmail.com>
2023-03-11 02:51:35 +0530

    f

--- src/components/core/Head.jsx
+++ src/components/core/Head.jsx
@@ -8,8 +8,6 @@ export const HeadComponent = ({title ="Home"}) => {
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-
-
     </>
   );
 };
