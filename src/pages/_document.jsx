6d43057ae324f2152a44534ee2ae89d022982e54
madhuribasant <108251406+madhuribasant@users.noreply.github.com>
2023-03-29 12:17:50 +0530

    hotjar code added

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -13,6 +13,18 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
           rel="stylesheet"
         />
+    
+      <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3428508,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+    
       </Head>
       <body className=" bg-black">
         <Main />
